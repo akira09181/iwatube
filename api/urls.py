c138292348763dc2b2394cbd169b1085f9edda89
Akira <freelance0918@gmail.com>
2022-02-27 16:46:24 +0900

    django api only done

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,14 @@ from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
 
+from api.views import VideoViewSet, CreateUserView
+
 router = routers.DefaultRouter()
+router.register('videos',VideoViewSet)
+
 
 urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
     path('', include(router.urls)),
 ]
 
